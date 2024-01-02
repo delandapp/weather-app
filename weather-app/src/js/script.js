f804ab261cc3b5e62aaa4f04eab3a472937ba56b
delandapp <alihandronikolas@gmail.com>
2024-01-03 00:44:45 +0700

    update jika data tidak ditemukan

--- weather-app/src/js/script.js
+++ weather-app/src/js/script.js
@@ -1,5 +1,28 @@
 const button = document.querySelector("button");
 const suhu = document.getElementById("suhu");
+const cuaca = document.getElementById("cuaca");
+const content = document.getElementById("content");
+const kotak = document.getElementById("kotak");
+
+fetch(
+  `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Jakarta?unitGroup=metric&key=RG3C38N9YUM9SLJNFAZX4NKKP&contentType=json`,
+  {
+    method: "GET",
+    headers: {},
+  }
+)
+  .then((response) => {
+    const data = response.json();
+    data.then((data) => {
+      suhu.innerHTML = data.currentConditions.temp + "°C";
+      cuaca.innerHTML = data.currentConditions.conditions;
+      content.style.display = "flex";
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 button.addEventListener("click", (e) => {
   e.preventDefault();
   const input = document.querySelector("input");
@@ -10,14 +33,18 @@ button.addEventListener("click", (e) => {
       method: "GET",
       headers: {},
     }
-  )
-    .then((response) => {
+  ).then((response) => {
       const data = response.json();
       data.then((data) => {
+        kotak.style.display = "flex";
+        kotak.previousElementSibling.style.display = "none";
         suhu.innerHTML = data.currentConditions.temp + "°C";
-      })
-    })
-    .catch((err) => {
-      console.error(err);
+        cuaca.innerHTML = data.currentConditions.conditions;
+      }).catch((err) => {
+        kotak.style.display = "none";
+        kotak.previousElementSibling.style.display = "block";
+      });
+    }).catch((err) => {
+        kotak.style.display = "none";
     });
 });
