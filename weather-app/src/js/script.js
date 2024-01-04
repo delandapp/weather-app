0e5b80a5d6cd91b08a8ac90e81d19e9e5b1d8b22
delandapp <alihandronikolas@gmail.com>
2024-01-04 22:16:39 +0700

    update humandity,temperature and wingspeed

--- weather-app/src/js/script.js
+++ weather-app/src/js/script.js
@@ -5,12 +5,13 @@ const content = document.getElementById("content");
 const kotak = document.getElementById("kotak");
 const loading = document.getElementById("loading");
 const icon = document.getElementById("icon");
+const dataKota = document.getElementById("data-kota");
 
 const dataicon = {
-    rain: "fa-solid fa-cloud-showers-heavy text-white text-[7rem]",
-    clear: "fa-solid fa-cloud text-white text-[7rem]",
-    Overcast: "fa-solid fa-cloud-meatball text-white text-[7rem]"
-}
+  rain: "fa-solid fa-cloud-showers-heavy text-white text-[7rem]",
+  clear: "fa-solid fa-cloud text-white text-[7rem]",
+  Overcast: "fa-solid fa-cloud-meatball text-white text-[7rem]",
+};
 
 fetch(
   `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Jakarta?unitGroup=metric&key=RG3C38N9YUM9SLJNFAZX4NKKP&contentType=json`,
@@ -23,9 +24,16 @@ fetch(
     const data = response.json();
     data.then((data) => {
       icon.setAttribute("class", dataicon[data.currentConditions.icon]);
-      suhu.previousElementSibling.previousElementSibling.innerHTML = data.resolvedAddress;
+      suhu.previousElementSibling.previousElementSibling.innerHTML =
+        data.resolvedAddress;
       suhu.innerHTML = data.currentConditions.temp + "°C";
       cuaca.innerHTML = data.currentConditions.conditions;
+      dataKota.children[0].children[1].innerHTML =
+        data.currentConditions.temp + "°C";
+      dataKota.children[1].children[1].innerHTML =
+        data.currentConditions.humidity + "%";
+      dataKota.children[2].children[1].innerHTML =
+        data.currentConditions.windspeed + "km/h";
       content.style.display = "flex";
     });
   })
@@ -51,15 +59,22 @@ button.addEventListener("click", (e) => {
       const data = response.json();
       data
         .then((data) => {
-          suhu.previousElementSibling.previousElementSibling.innerHTML = data.resolvedAddress;
+          suhu.previousElementSibling.previousElementSibling.innerHTML =
+            data.resolvedAddress;
           loading.style.display = "none";
           kotak.style.display = "flex";
           loading.previousElementSibling.style.display = "none";
           suhu.innerHTML = data.currentConditions.temp + "°C";
           cuaca.innerHTML = data.currentConditions.conditions;
+          dataKota.children[0].children[1].innerHTML =
+            data.currentConditions.temp + "°C";
+          dataKota.children[1].children[1].innerHTML =
+            data.currentConditions.humidity + "%";
+          dataKota.children[2].children[1].innerHTML =
+            data.currentConditions.windspeed + "km/h";
         })
         .catch((err) => {
-        console.log("error")
+          console.log("error");
           loading.style.display = "none";
           kotak.style.display = "none";
           loading.previousElementSibling.style.display = "block";
